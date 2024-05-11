4dc5e7b8ef7d935043b1d7ee3ff53e72450a30e6
Bad Dog <apatrushko@yahoo.com>
2024-05-11 09:21:04 -0400

    Version bump

--- FO4Edit/Edit Scripts/FFO_Furrifier.pas
+++ FO4Edit/Edit Scripts/FFO_Furrifier.pas
@@ -25,7 +25,7 @@ uses FFO_RaceProbabilities, BDFurryArmorFixup, FFOGenerateNPCs, BDScriptTools,
 BDAssetLoaderFO4, xEditAPI, Classes, SysUtils, StrUtils, Windows, Forms;
 
 const
-    FURRIFIER_VERSION = '2.11';
+    FURRIFIER_VERSION = '2.12';
     SHOW_OPTIONS_DIALOG = True;
     PATCH_FILE_NAME = 'FFOPatch.esp'; // Set to whatever
     USE_SELECTION = FALSE;           // FALSE or TRUE
